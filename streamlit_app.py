56b24bb22a0d638743aac39654152d476a4aba31
omkardiwakar <102148165+omkardiwakar@users.noreply.github.com>
2023-12-28 09:58:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,26 +61,37 @@ try:
 except URLError as e:
   streamlit.error()
 # import snowflake.connector
+streamlit.header("The fruit load list contains:")
+# snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall();
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+# add a button to load the fruit
+if streamlit.button('Get Fruit Load List')
+
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    # my_cur = my_cnx.cursor()
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-my_cur.execute("select * from fruit_load_list")
+# my_cur.execute("select * from fruit_load_list")
+# # my_data_row = my_cur.fetchone()
+# my_data_row = my_cur.fetchall()
+# streamlit.text("Hello from Snowflake:")
+# streamlit.text("the fruit load list contains:")
+# streamlit.text(my_data_row)
+
 # my_data_row = my_cur.fetchone()
-my_data_row = my_cur.fetchall()
-streamlit.text("Hello from Snowflake:")
-streamlit.text("the fruit load list contains:")
-streamlit.text(my_data_row)
 
-my_data_row = my_cur.fetchone()
-
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+# my_cur.execute("select * from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
 
 
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
 
 
 
