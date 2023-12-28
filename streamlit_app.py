2d2fec12684057fc8af0c3747b424c79f382e535
omkardiwakar <102148165+omkardiwakar@users.noreply.github.com>
2023-12-28 10:00:49 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,19 +79,19 @@ if streamlit.button('Get Fruit Load List'):
 
 # my_cur.execute("select * from fruit_load_list")
 # my_data_row = my_cur.fetchone()
-my_data_row = my_cur.fetchall()
+# my_data_row = my_cur.fetchall()
 
-streamlit.text("the fruit load list contains:")
-streamlit.text(my_data_row)
+# streamlit.text("the fruit load list contains:")
+# streamlit.text(my_data_row)
 
-my_data_row = my_cur.fetchone()
+# my_data_row = my_cur.fetchone()
 
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+# my_cur.execute("select * from fruit_load_list")
+# my_data_rows = my_cur.fetchall()
 
 
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_row)
+# streamlit.header("The fruit load list contains:")
+# streamlit.dataframe(my_data_row)
 
 
 
