ff5d914ffbbe8f7c9607b065dc779017f392d973
omkardiwakar <102148165+omkardiwakar@users.noreply.github.com>
2023-12-28 10:33:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,30 +69,30 @@ def get_fruit_load_list():
          return my_cur.fetchall();
 
 
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ( 'from streamlit')")
-        return "Thanks for adding" + new_fruit
-
 # def insert_row_snowflake(new_fruit):
-#      with my_cnx.cursor() as my_cur:
-#         my_cur.execute("insert into fruit_load_list values (' " +  + " ')")
+#     with my_cnx.cursor() as my_cur:
+#         my_cur.execute("insert into fruit_load_list values ( 'from streamlit')")
 #         return "Thanks for adding" + new_fruit
+
+def insert_row_snowflake(new_fruit):
+     with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values (' " +  + " ')")
+        return "Thanks for adding" + new_fruit
 
 
 add_my_fruit = streamlit.text_input('what fruit would you like to add?')
 # add a button to load the fruit
-if streamlit.button('Add a Fruit to the List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    streamlit.text(back_from_function) 
+# if streamlit.button('Add a Fruit to the List'):
+#     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#     back_from_function = insert_row_snowflake(add_my_fruit)
+#     streamlit.text(back_from_function) 
 
 # *************************************************************
-# if streanlit.button('Get Fruit List'):
-#     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-#     my_data_rows = get_fruit_load_list()
-#     my_cnx.close()
-#     streamlit.dataframe(my_data_rows)
+if streanlit.button('Get Fruit List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
   
 
 
